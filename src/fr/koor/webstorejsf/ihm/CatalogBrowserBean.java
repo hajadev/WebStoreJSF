0ff4f351430b5ef7005fa522d0a0391c314084b0
hajadev <haja.dev@gmail.com>
2020-03-19 21:10:43 +0100

    Listeners implicit implementation

--- src/fr/koor/webstorejsf/ihm/CatalogBrowserBean.java
+++ src/fr/koor/webstorejsf/ihm/CatalogBrowserBean.java
@@ -2,6 +2,7 @@ package fr.koor.webstorejsf.ihm;
 import java.io.Serializable;
 
 import javax.enterprise.context.SessionScoped;
+import javax.faces.event.ActionEvent;
 import javax.inject.Named;
 
 @Named
@@ -21,4 +22,12 @@ public class CatalogBrowserBean implements Serializable {
         this.index = index;
     }
     
+    public void processPreviousAction( ActionEvent event ) {
+        index--;
+    }
+    
+    public void processNextAction( ActionEvent event ) {
+        index++;
+    }
+
 }
