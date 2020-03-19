5bcf599c196cdc4f596f53f19f8dd89af4f32ed0
hajadev <haja.dev@gmail.com>
2020-03-19 12:30:30 +0100

    First JSF page/form with action on success

---
+++ src/fr/koor/webstorejsf/ApplicationConfiguration.java
@@ -0,0 +1,10 @@
+package fr.koor.webstorejsf;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+@ApplicationScoped
+@FacesConfig( version = FacesConfig.Version.JSF_2_3 )         // Activation de CDI
+public class ApplicationConfiguration {
+    
+}
